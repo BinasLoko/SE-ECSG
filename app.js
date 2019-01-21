1af22441b1b4f6249e4dbee3ab87ab9d73eefd58
Paulo Vale <paulovale@madeinweb.com.br>
2019-01-21 20:24:01 -0200

    Added an empty public folder to the project and added it as a static file folder to the api.

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 'use strict';
 
+/* Settings */
 const express = require('express');
 const app = express();
-const port = 8000;  
+const port = 8000;
 
+/* Middlewares */
+app.use(express.static('public'))
+
+/* Routing */
 app.get('/', (req, res) => res.send('Hello World!'))
 
+/* Server */
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
