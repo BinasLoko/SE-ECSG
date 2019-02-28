8664f87d5044576f09c183bd69c628bb28399bd8
unknown <mateustcem@hotmail.com>
2019-02-28 20:42:29 -0300

    fazendo login no sistema

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -11,7 +11,7 @@ function execSQLQuery(sqlQry, queryValues) {
         database: 'expertsystemdb'
     });
     return new Promise((resolve, reject) => {
-        connection.query(sqlQry, queryValues,  function (error, results, fields) {
+        connection.query(sqlQry, queryValues, function (error, results, fields) {
 
             connection.end();
             if (error) return reject(error);
@@ -42,7 +42,7 @@ router.get('/register', (req, res) =>
 router.post('/register', (req, res) => {
     const body_values = req.body;
     console.log(body_values);
-        
+
     let query = `INSERT INTO pessoas 
         (   
             nome, 
@@ -60,26 +60,26 @@ router.post('/register', (req, res) => {
             ?,?,?,?,?,?,?,?,?
         )`;
 
-        var teste = body_values.nome;
-      let form_values = [
-            body_values.nome,
-            body_values.sobrenome,
-            body_values.pais,
-            body_values.sexo,
-            body_values.telefone,
-            body_values.nascimento,
-            body_values.username,
-            body_values.password,
-            body_values.email        
-      ]
+    var teste = body_values.nome;
+    let form_values = [
+        body_values.nome,
+        body_values.sobrenome,
+        body_values.pais,
+        body_values.sexo,
+        body_values.telefone,
+        body_values.nascimento,
+        body_values.username,
+        body_values.password,
+        body_values.email
+    ]
 
     execSQLQuery(query, form_values)
-    .then(dbResponse => {
-        res.redirect('/login');
-    })
-    .catch(error => {
-       res.json('fuck'); 
-    });
+        .then(dbResponse => {
+            res.redirect('/login');
+        })
+        .catch(error => {
+            res.json('fuck');
+        });
 
     //todo
     //se der erro, botar as infos no localstorage e atribuir 
@@ -94,16 +94,31 @@ module.exports = router;
 
 router.get('/list', async (req, res) => {
     const dbResponse = await execSQLQuery('select * from pessoas');
-res.json(dbResponse);
+    res.json(dbResponse);
 })
 
-router.post('/login', (req, res) =>{
+router.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    
+
+    let query = `
+            SELECT * 
+            FROM pessoas
+            WHERE nome_usuario = ? 
+            AND senha_usuario = ?`;
+
+    let form_values = [username, password];
+
     if(username && password){
-        
+        execSQLQuery(query, form_values)
+        .then(dbResponse => {
+            res.redirect('/home');
+        })
+        .catch(error => {
+            res.json('fuck');
+        });
     }
+
 
 
 });
