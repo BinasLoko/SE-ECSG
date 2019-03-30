9d2b58afcdbbe943ef1c41766ed9709e5488d180
unknown <mateustcem@hotmail.com>
2019-03-29 21:30:32 -0300

    pegando as respostas dos radiobutton, colocando no localstorage e removendo

--- public/js/inference.js
+++ public/js/inference.js
@@ -5,8 +5,22 @@ $(document).ready(function () {
 
         $('.btn-final').on('click', function () {
 
-            let heuristic_one = $('.heuristic-one').val();
+            let heuristic_one = $('[name=likert-one]:checked').val();
 
+            let heuristic_three = $('[name=likert-three]:checked').val();
+            
+            let heuristic_five = $('[name=likert-five]:checked').val();
+            
+            let heuristic_list = [{heuristic_one}, {heuristic_three}, {heuristic_five}];
+            
+            
+
+            localStorage.setItem('heuristic_values', JSON.stringify(heuristic_list));
+            let heuristic_result = localStorage.getItem('heuristic_values');
+            console.log(JSON.parse(heuristic_result));
+            localStorage.removeItem('heuristic_values');
+
+            
 
         });
 
